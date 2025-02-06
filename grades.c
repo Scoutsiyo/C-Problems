a032a5ffe48f7880c74a3b213446cc5581322951
Scoutsiyo <117695786+Scoutsiyo@users.noreply.github.com>
2025-02-06 17:14:47 -0500

    Grades.c
    checks if you pass

---
+++ grades.c
@@ -0,0 +1,28 @@
+  //
+  // Created by yul on 2/3/2025.
+  //
+
+  #include <stdio.h>
+  int main(){
+    int grade1,grade2,grade3;
+
+    int promedio;
+    printf("Enter first grade: \n");
+
+
+    scanf("%d", &grade1);
+
+    printf("Enter second grade: \n");
+    scanf("%d", &grade2);
+    printf("Enter third grade: \n");
+    scanf("%d", &grade3);
+
+    promedio = (grade1 + grade2 + grade3) / 3;
+
+    if (promedio >= 7) {
+      printf("Grade A\n");
+    }
+    else {
+      printf("Grade B\n");
+    }
+  }
