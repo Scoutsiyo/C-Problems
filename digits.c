eeb73323ec932ba9ad1a319791e33cdb5f803235
Scoutsiyo <117695786+Scoutsiyo@users.noreply.github.com>
2025-02-07 01:29:14 +0000

    Solved problem

--- digits.c
+++ digits.c
@@ -1,1 +1,36 @@
+/*
+Given a five digit integer, print the sum of its digits.
 
+Input Format
+
+The input contains a single five digit number, .
+
+Constraints
+
+
+Output Format
+
+Print the sum of the digits of the five digit number.
+
+Sample Input 0
+
+10564
+Sample Output 0
+
+16
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int digits[5];
+    for(int i = 0; i < 5; i++)
+    {
+        digits[i] = n % 10;
+        n /= 10;
+    }
+    printf("%d\n",digits[0]+digits[1]+digits[2]+digits[3]+digits[4]);
+    return 0;
+}
